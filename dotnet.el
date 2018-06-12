a7f782cbde05d7acd432d637428de881c957b303
EAS7510 <michaeleaster@northwesternmutual.com>
2018-06-11 22:20:06 -0500

    .NET stuff.

--- dotnet.el
+++ dotnet.el
@@ -1,32 +1,52 @@
 (require 'tools-common)
 
-(defun dotnet-in-project (f)
-  (let ((default-directory (tools-common-find-containing-directory-upwards "project.clj")))
+(defun dot-in-project (f)
+  (let ((default-directory (tools-common-find-containing-directory-upwards ".*.csproj")))
     (if default-directory
       (funcall f default-directory)
       (error "Not inside a dotnet project!"))))
 
-(defun dotnet-command ()
-  (dotnet-in-project (lambda (d) (compile (concat "dotnet " cmd)))))
+(defun dot-command (cmd)
+  (dot-in-project (lambda (d) (compile (concat "dotnet " cmd)))))
 
-(defun dotnet-restore ()
+(defun dot-restore ()
   (interactive)
-  (dotnet-command "restore"))
+  (dot-command "restore"))
 
-(defun dotnet-build ()
+(defun dot-build ()
   (interactive)
-  (dotnet-command "build"))
+  (dot-command "build"))
 
-(defvar dotnet-grep-history nil)
-(defun dotnet-grep (term)
+(defun dot-suite ()
+  (interactive)
+  (let ((test-name (file-name-base (buffer-file-name))))
+    (dot-command (concat "test --filter \"FullyQualifiedName~" test-name ".\""))))
+
+(defvar dot-grep-history nil)
+(defun dot-grep (term)
   (interactive
    (list
-    (let* ((default-term (car dotnet-grep-history))
+    (let* ((default-term (car dot-grep-history))
            (prompt (if (s-blank? default-term)
                      "Search for: "
                      (concat "Search string (default \"" default-term "\"): ")))
-           (input (read-from-minibuffer prompt nil nil nil 'dotnet-grep-history)))
+           (input (read-from-minibuffer prompt nil nil nil 'dot-grep-history)))
       (if (s-blank? input) default-term input))))
-  (dotnet-in-project
+  (dot-in-project
    (lambda (d)
-     (grep (concat "grep --color --exclude=\*.{js,map,json} --exclude-dir=.git --exclude-dir=target -nriH -e \"" term "\" " d)))))
+     (grep (concat "grep --color --exclude=\*.{js,map,json,dll,xml} --exclude-dir=.git --exclude-dir=bin -nriH -e \"" term "\" " d)))))
+
+(defvar dot-test-history nil)
+(defun dot-test (test)
+  (interactive
+   (list
+    (let* ((default-term (thing-at-point 'symbol))
+           (prompt (if (s-blank? default-term)
+                     "Run test: "
+                     (concat "Run test (default \"" default-term "\"): ")))
+           (input (read-from-minibuffer prompt nil nil nil 'mvn-test-history)))
+      (if (s-blank? input) default-term input))))
+  (dot-in-project
+   (lambda (d)
+     (let ((test-name (file-name-base (buffer-file-name))))
+       (compile (concat "dotnet test --filter \"FullyQualifiedName~"  test-name "." test "\""))))))
